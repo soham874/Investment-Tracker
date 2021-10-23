5efc4d10f320b075beb0db2a85cde1e786cfe93c
soham874 <soham874@gmail.com>
2021-10-23 13:10:30 +0530

    Added : Skeleton Login file

---
+++ src/Pages/Login/login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export deafult class loginPage extends React.Component{
+
+    constructor(props){
+	super(props)
+	this.state = {
+	    userName: React.createRef(),
+	    password: React.createRef()
+	}
+    }
+
+    render(){
+	return{
+	    
+	}
+    }
+
+}
